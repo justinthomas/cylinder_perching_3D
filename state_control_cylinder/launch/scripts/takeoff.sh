e6e3273adc75d0db67f17e1a819bef698cb180e4
justinthomas <justinthomas@jtwebs.net>
2014-09-24 21:12:25 -0400

    Successful implementation of vision based control!

--- state_control_cylinder/launch/scripts/takeoff.sh
+++ state_control_cylinder/launch/scripts/takeoff.sh
@@ -4,14 +4,20 @@
 echo "Pressing Record..."
 rosrun korg_nanokontrol2 kontrol_sim.py 28
 
+sleep 1
+
 # Play
 echo "Pressing Play..."
 rosrun korg_nanokontrol2 kontrol_sim.py 27
+
+sleep 1
 
 # Set (hover)
 echo "Pressing Set (to hover)..."
 rosrun korg_nanokontrol2 kontrol_sim.py 31
 
+sleep 1
+
 # Line Tracker to {0, 0, 1}
-echo "Line Tracker to {0, 0, 1}"
-rosrun korg_nanokontrol2 kontrol_sim.py 29 0 0 0.25
+echo "LineTrackerYaw to {0, 0, 1}"
+rosrun korg_nanokontrol2 kontrol_sim.py 24 0 0 0.25
--- state_control_cylinder/src/state_control.cpp
+++ state_control_cylinder/src/state_control.cpp
@@ -22,6 +22,7 @@
 #include <controllers_manager/Transition.h>
 #include <quadrotor_msgs/FlatOutputs.h>
 #include <quadrotor_msgs/PositionCommand.h>
+#include <quadrotor_msgs/SO3Command.h>
 #include "nano_kontrol2.h"
 // #include <trajectory.h>
 #include <cylinder_msgs/ImageFeatures.h>
@@ -32,6 +33,14 @@ using namespace Eigen;
 using namespace tf;
 
 #define SAFETY_ON
+
+#define TEXT_RED "\e[91m"
+#define TEXT_GREEN "\e[92m"
+#define TEXT_YELLOW "\e[93m"
+#define TEXT_BLUE "\e[94m"
+#define TEXT_MAGENTA "\e[95m"
+#define TEXT_CYAN "\e[96m"
+#define TEXT_RESET "\e[0m"
 
 enum controller_state
 {
@@ -82,12 +91,15 @@ static ros::Publisher pub_goal_yaw_;
 static ros::Publisher pub_info_bool_;
 static ros::ServiceClient srv_transition_;
 static ros::Publisher pub_vision_status_;
+static ros::Publisher so3_command_pub_;
 
 // Vision Stuff
-static tf::Transform T_Cam_to_Body_ = tf::Transform(tf::Matrix3x3(1,0,0, 0,-1,0, 0,0,-1), tf::Vector3(0,0,0));
-static void Jacobians(const Vector3d &P1_in_pp, const Vector3d &sdot, Matrix3d &J, Matrix3d &Jinv, Matrix3d &Jdot);
-// static void sim_pp_features(const double &r, const Eigen::Vector3d &P1_in_pp, const Eigen::Vector3d &P0, Eigen::Vector3d &s);
+// static tf::Transform T_Cam_to_Body_ = tf::Transform(tf::Matrix3x3(1,0,0, 0,-1,0, 0,0,-1), tf::Vector3(0,0,0));
+static void Jacobians(const Vector3d &P1_inV, const Vector3d &sdot, Matrix3d &J, Matrix3d &Jinv, Matrix3d &Jdot);
+// static void sim_pp_features(const double &r, const Eigen::Vector3d &P1_inV, const Eigen::Vector3d &P0, Eigen::Vector3d &s);
 double r;
+float kR_[3], kOm_[3], corrections_[3];
+bool enable_motors_, use_external_yaw_;
 
 // Quadrotor Pose
 static geometry_msgs::Point pos_;
@@ -101,6 +113,7 @@ static const std::string line_tracker_distance("line_tracker/LineTrackerDistance
 static const std::string line_tracker_min_jerk("line_tracker/LineTrackerMinJerk");
 static const std::string line_tracker_yaw("line_tracker/LineTrackerYaw");
 static const std::string velocity_tracker_str("velocity_tracker/VelocityTrackerYaw");
+static const std::string null_tracker_str("null_tracker/NullTracker");
 
 // Function Declarations
 void hover_in_place();
@@ -108,7 +121,6 @@ void hover_at(const geometry_msgs::Point goal);
 void go_home();
 void go_to(const quadrotor_msgs::FlatOutputs goal);
 void print_tfVector3(tf::Vector3 vec);
-void fmtVec3d(std::string &str, const Eigen::Vector3d &v);
 void color(std::string &str);
 
 // Callbacks and functions
@@ -316,6 +328,12 @@ static void nanokontrol_cb(const sensor_msgs::Joy::ConstPtr &msg)
     // Vision Control
     else if(selected && msg->buttons[vision_control_button] && state_ == HOVER && vision_info_ && imu_info_)
     {
+      ROS_INFO("Activating Vision Control.  state_ == VISION_CONTROL;");
+
+      // pub_goal_trajectory_.publish(traj_goal_);
+      controllers_manager::Transition transition_cmd;
+      transition_cmd.request.controller = null_tracker_str;
+      srv_transition_.call(transition_cmd);
       state_ = VISION_CONTROL;
     }
   }
@@ -397,54 +415,203 @@ static void image_update_cb(const cylinder_msgs::ParallelPlane::ConstPtr &msg)
 {
   vision_info_ = true;
 
-  // The rotation from the parallel plane to the world
-  Matrix3d R_pp_to_W(Quaterniond(msg->q.w, msg->q.x, msg->q.y, msg->q.z));
+  // Extract stuff from the message
+  Eigen::Matrix3d R_VtoW(Eigen::Quaterniond(msg->qVtoW.w, msg->qVtoW.x, msg->qVtoW.y, msg->qVtoW.z));
+  // cout << TEXT_RED << "R_VtoW = " << endl << R_VtoW << TEXT_RESET << endl;
+
+  // tf::Matrix3x3 R_CtoW(tf::Quaternion(msg->qCtoW.x, msg->qCtoW.y, msg->qCtoW.z, msg->qCtoW.w));
+  Eigen::Matrix3d R_CtoW(Eigen::Quaterniond(msg->qCtoW.w, msg->qCtoW.x, msg->qCtoW.y, msg->qCtoW.z));
+
+  // Eigen::Matrix3d R_BtoW(Eigen::Quaterniond(msg->qBtoW.w, msg->qBtoW.x, msg->qBtoW.y, msg->qBtoW.z));
+  tf::Quaternion qBtoW(msg->qBtoW.x, msg->qBtoW.y, msg->qBtoW.z, msg->qBtoW.w);
+  double current_yaw = tf::getYaw(qBtoW);
+
   Vector3d P1(msg->P1.x, msg->P1.y, msg->P1.z);
+  Vector3d s(msg->s[0], msg->s[1], msg->s[2]);
   Vector3d sdot(msg->sdot[0], msg->sdot[1], msg->sdot[2]);
 
   Matrix3d J, Jdot, Jinv;
   Jacobians(P1, sdot, J, Jinv, Jdot);
 
-  // Vector3d vel_world = R_pp_to_W * Jinv * sdot;
-  Vector3d vel_world = Jinv * sdot;
-  gstr = "World Vel Est";
-  fmtVec3d(gstr, vel_world);
-  ROS_INFO_THROTTLE(1, gstr.c_str());
+  Matrix3d T = R_VtoW * Jinv;
+  // cout << TEXT_CYAN << "Jinv = " << endl << Jinv << TEXT_RESET << endl;
+  // cout << TEXT_BLUE << "T = " << endl << T << TEXT_RESET << endl;
+
+  // Optional
+  // Vector3d vel_world = T * sdot;
+  // ROS_INFO_THROTTLE(1, TEXT_YELLOW "Actual Velocity:            {%2.2f, %2.2f, %2.2f}" TEXT_RESET, vel_.x, vel_.y, vel_.z);
+  // ROS_INFO_THROTTLE(1, TEXT_CYAN   "Velocity Estimate in World: {%2.2f, %2.2f, %2.2f}" TEXT_RESET, vel_world(0), vel_world(1), vel_world(2));
+  
+  // These will eventually be set by a trajectory
+  Vector3d sdes(0.1,0.1,0), sdotdes(0,0,0), sddotdes(0,0,0), sdddotdes(0,0,0);
+
+  // Useful defs
+  double g = 9.81;
+  static const Vector3d e3(0,0,1);
+
+  // Errors
+  Vector3d e_pos(sdes - s), e_vel(sdotdes - sdot);
+  
+  // ROS_INFO_THROTTLE(1, TEXT_MAGENTA "s    = {%2.2f, %2.2f, %2.2f}" TEXT_RESET, s(0), s(1), s(2));
+  // ROS_INFO_THROTTLE(1, TEXT_MAGENTA "sdes = {%2.2f, %2.2f, %2.2f}" TEXT_RESET, sdes(0), sdes(1), sdes(2));
+  // ROS_INFO_THROTTLE(1, "\e[91me_pos: {%2.2f, %2.2f, %2.2f}\e[0m", e_pos(0), e_pos(1), e_pos(2));
+  // ROS_INFO_THROTTLE(1, "\e[93me_vel: {%2.2f, %2.2f, %2.2f}\e[0m", e_vel(0), e_vel(1), e_vel(2));
+
+  /*
+  for (int i=0; i<=1; i++)
+  {
+    if (s(i) < 0)
+    {
+      ROS_INFO("Switched index %d", i);
+      e_pos(i) = -e_pos(i);
+      e_vel(i) = -e_vel(i);
+    }
+  }
+  */
+
+  // Gains
+  double kprho(1), kpu(1), kdrho(1), kdu(1);
+  Vector3d kx, kv;
+  // kx << kprho, kprho, kpu;
+  kx << 1, 1, 1;
+  kv << kdrho, kdrho, kdu;
+
+  // Temp output
+  // Vector3d temp = mass_ * R_VtoW * Jinv * (kx.asDiagonal() * e_pos); //  + kv.asDiagonal() * e_vel + sddotdes);
+  // ROS_INFO_THROTTLE(1, "\e[93mdelta_force_in_pp: {%2.2f, %2.2f, %2.2f}\e[0m", temp(0), temp(1), temp(2));
+
+  // Nominal thrust (in the world)
+  Vector3d force = mass_ * g * e3 + mass_ * (
+      kx.asDiagonal() * R_VtoW * Jinv * e_pos + kv.asDiagonal() * R_VtoW * Jinv * e_vel + sddotdes);
+  // ROS_INFO_THROTTLE(1, TEXT_GREEN "force: {%2.2f, %2.2f, %2.2f}" TEXT_RESET, force(0), force(1), force(2));
+  
+  // Vector3d force1 = mass_ * kx.asDiagonal() * T * e_pos;
+  // ROS_INFO_THROTTLE(1, TEXT_GREEN "Position component of force: {%2.2f, %2.2f, %2.2f}" TEXT_RESET, force1(0), force1(1), force1(2));
+  // Vector3d force2 = mass_ * kv.asDiagonal() * T * e_vel;
+  // ROS_INFO_THROTTLE(1, TEXT_RED "Velocity component of force: {%2.2f, %2.2f, %2.2f}" TEXT_RESET, force2(0), force2(1), force2(2));
+
+  double des_yaw(0), des_yaw_dot(0);
+  Eigen::Vector3d b1c, b2c, b3c;
+  Eigen::Vector3d b1d(cos(des_yaw), sin(des_yaw), 0);
+
+  if(force.norm() > 1e-6)
+    b3c.noalias() = force.normalized();
+  else
+    b3c.noalias() = Eigen::Vector3d(0, 0, 1);
+
+  b2c.noalias() = b3c.cross(b1d).normalized();
+  b1c.noalias() = b2c.cross(b3c).normalized();
+
+  const Eigen::Vector3d force_dot = mass_ * R_VtoW * (
+      Jinv * (kx.asDiagonal()*e_vel + sdddotdes)
+      - Jinv * Jdot * Jinv * (kx.asDiagonal() * e_pos + kv.asDiagonal() * e_vel + sddotdes)); // Ignoring kv*e_acc and ki*e_pos terms
+  const Eigen::Vector3d b3c_dot = b3c.cross(force_dot/force.norm()).cross(b3c);
+  const Eigen::Vector3d b1d_dot(-sin(des_yaw)*des_yaw_dot, cos(des_yaw)*des_yaw_dot, 0);
+  const Eigen::Vector3d b2c_dot = b3c_dot.cross(b1d) + b3c.cross(b1d_dot);
+  const Eigen::Vector3d b1c_dot = b2c_dot.cross(b3c) + b2c.cross(b3c_dot);
+
+  Eigen::Matrix3d Rc;
+  Rc << b1c, b2c, b3c;
+  Eigen::Quaterniond qdes(Rc);
+
+  Eigen::Matrix3d R_dot;
+  R_dot << b1c_dot, b2c_dot, b3c_dot;
+
+  const Eigen::Matrix3d omega_hat = Rc.transpose()*R_dot;
+  Eigen::Vector3d angular_velocity = Eigen::Vector3d(omega_hat(2,1), omega_hat(0,2), omega_hat(1,0));
+
+  // Only publish if we are in the vision control state
+  if (state_ == VISION_CONTROL)
+  {
+    quadrotor_msgs::SO3Command::Ptr cmd(new quadrotor_msgs::SO3Command);
+    cmd->header.stamp = msg->stamp;
+    // cmd->header.frame_id = 0;
+    cmd->force.x = force(0);
+    cmd->force.y = force(1);
+    cmd->force.z = force(2);
+    cmd->orientation.x = qdes.x();
+    cmd->orientation.y = qdes.y();
+    cmd->orientation.z = qdes.z();
+    cmd->orientation.w = qdes.w();
+    cmd->angular_velocity.x = angular_velocity(0);
+    cmd->angular_velocity.y = angular_velocity(1);
+    cmd->angular_velocity.z = angular_velocity(2);
+    for(int i = 0; i < 3; i++)
+    {
+      cmd->kR[i] = kR_[i];
+      cmd->kOm[i] = kOm_[i];
+    }
+    cmd->aux.current_yaw = current_yaw;
+    cmd->aux.kf_correction = corrections_[0];
+    cmd->aux.angle_corrections[0] = corrections_[1];
+    cmd->aux.angle_corrections[1] = corrections_[2];
+    cmd->aux.enable_motors = true;
+    cmd->aux.use_external_yaw = true; // use_external_yaw_;
+
+    so3_command_pub_.publish(cmd);
+    ROS_INFO_THROTTLE(1, "Vision control");
+  }
 }
 
-void Jacobians(const Vector3d &P1_in_pp, const Vector3d &sdot, Matrix3d &J, Matrix3d &Jinv, Matrix3d &Jdot)
+void Jacobians(const Vector3d &P1_inV, const Vector3d &sdot, Matrix3d &J, Matrix3d &Jinv, Matrix3d &Jdot)
 {
+  // These values are correct in the virtual frame
+  // ROS_INFO_THROTTLE(1, "\e[94mUsing {x1, y1, z1, r} = {%2.2f, %2.2f, %2.2f, %2.2f} in V\e[0m", P1_inV(0), P1_inV(1), P1_inV(2), r);
+
+  // Matrix to change convention
+  Eigen::Matrix3d Rcc;
+  Rcc << -1,0,0, 0,-1,0, 0,0,1;
+
   double x1, y1, z1;
-  x1 = P1_in_pp(0);
-  y1 = P1_in_pp(1);
-  z1 = P1_in_pp(2);
+  Eigen::Vector3d P1_cc = Rcc * P1_inV;
+  x1 = P1_cc(0);
+  y1 = P1_cc(1);
+  z1 = P1_cc(2);
 
+  // These values are correct in the virtual frame
+  // ROS_INFO_THROTTLE(1, "\e[93mUsing {x1, y1, z1, r} = {%2.2f, %2.2f, %2.2f, %2.2f} to compute J\e[0m", x1, y1, z1, r);
+
+  // Unmodified Jacobian (already assumed that axis = {-1, 0, 0})
   J(0,0) = 0;
-  J(0,1) = (pow(y1,2) + pow(z1,2))/((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) - r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)));
-  J(0,2) = -(((pow(y1,2) + pow(z1,2))*(r*z1 + y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/(sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*pow(r*y1 - z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2)));
+  J(0,1) = -((pow(y1,2) + pow(z1,2))/((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) + r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))));
+  J(0,2) = ((pow(y1,2) + pow(z1,2))*(-(r*z1) + y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/(sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*pow(r*y1 + z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2));
   J(1,0) = 0;
-  J(1,1) = -((pow(y1,2) + pow(z1,2))/((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) + r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))));
-  J(1,2) = ((pow(y1,2) + pow(z1,2))*(-(r*z1) + y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/(sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*pow(r*y1 + z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2));
-  J(2,0) = -(1/z1);
+  J(1,1) = (pow(y1,2) + pow(z1,2))/((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) - r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)));
+  J(1,2) = -(((pow(y1,2) + pow(z1,2))*(r*z1 + y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/(sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*pow(r*y1 - z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2)));
+  J(2,0) = 1/z1;
   J(2,1) = 0;
-  J(2,2) = x1/pow(z1,2);
+  J(2,2) = -(x1/pow(z1,2));
+  
+  // cout << TEXT_CYAN << "J = " << endl << J << TEXT_RESET << endl;
 
-  Jinv = J.inverse();
-  Vector3d p1dot = Jinv * sdot;
+  // Make sure we switch back to the correct convention
+  // Note: The minus sign gives us the velocity of the robot relative to P1
+  // instead of the velocity of P1 relative to the robot
+  Jinv = - Rcc.inverse() * J.inverse();
+
+  // Estimate the world velocity (minus to counter the above switch)
+  Vector3d p1dot = - Jinv * sdot;
+
+  // Change back into the old convention
+  p1dot = Rcc * p1dot;
   double x1dot, y1dot, z1dot;
   x1dot = p1dot(0);
   y1dot = p1dot(1);
   z1dot = p1dot(2);
-
+  
+  // Unmodified (already assumed that axis = {-1, 0, 0})
   Jdot(0,0) = 0;
-  Jdot(0,1) = (2*((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) - r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)))*(y1*y1dot + z1*z1dot) - (pow(y1,2) + pow(z1,2))*(2*y1*z1*y1dot - r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (-pow(r,2) + pow(y1,2))*z1dot + 3*pow(z1,2)*z1dot - (r*y1*(y1*y1dot + z1*z1dot))/sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/pow((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) - r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2);
-  Jdot(0,2) = (-(pow(y1,6)*z1*y1dot) + 2*pow(y1,7)*z1dot + pow(y1,5)*(r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot - 3*(pow(r,2) - 2*pow(z1,2))*z1dot) + 3*pow(y1,4)*z1*((pow(r,2) - pow(z1,2))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*z1dot) + pow(z1,3)*((pow(r,4) - pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(pow(r,2) + 2*pow(z1,2))*z1dot) + pow(y1,2)*z1*(-3*(pow(r,4) - pow(r,2)*pow(z1,2) + pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(-3*pow(r,2) + 5*pow(z1,2))*z1dot) + y1*pow(z1,2)*(3*pow(r,3)*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (-3*pow(r,4) + 2*pow(z1,4))*z1dot) + pow(y1,3)*(r*(-pow(r,2) + pow(z1,2))*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (pow(r,4) - 3*pow(r,2)*pow(z1,2) + 6*pow(z1,4))*z1dot))/(pow(-pow(r,2) + pow(y1,2) + pow(z1,2),1.5)*pow(-(r*y1) + z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),3));
+  Jdot(0,1) = (-2*((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) + r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)))*(y1*y1dot + z1*z1dot) + (pow(y1,2) + pow(z1,2))*(2*y1*z1*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (-pow(r,2) + pow(y1,2))*z1dot + 3*pow(z1,2)*z1dot + (r*y1*(y1*y1dot + z1*z1dot))/sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/pow((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) + r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2);
+  Jdot(0,2) = (pow(y1,6)*z1*y1dot - 2*pow(y1,7)*z1dot + pow(y1,5)*(r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + 3*(pow(r,2) - 2*pow(z1,2))*z1dot) + 3*pow(y1,4)*z1*((-pow(r,2) + pow(z1,2))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*z1dot) + pow(z1,3)*((-pow(r,4) + pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(pow(r,2) + 2*pow(z1,2))*z1dot) + pow(y1,2)*z1*(3*(pow(r,4) - pow(r,2)*pow(z1,2) + pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(-3*pow(r,2) + 5*pow(z1,2))*z1dot) + y1*pow(z1,2)*(3*pow(r,3)*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (3*pow(r,4) - 2*pow(z1,4))*z1dot) - pow(y1,3)*(r*(pow(r,2) - pow(z1,2))*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (pow(r,4) - 3*pow(r,2)*pow(z1,2) + 6*pow(z1,4))*z1dot))/(pow(-pow(r,2) + pow(y1,2) + pow(z1,2),1.5)*pow(r*y1 + z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),3));
   Jdot(1,0) = 0;
-  Jdot(1,1) = (-2*((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) + r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)))*(y1*y1dot + z1*z1dot) + (pow(y1,2) + pow(z1,2))*(2*y1*z1*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (-pow(r,2) + pow(y1,2))*z1dot + 3*pow(z1,2)*z1dot + (r*y1*(y1*y1dot + z1*z1dot))/sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/pow((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) + r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2);
-  Jdot(1,2) = (pow(y1,6)*z1*y1dot - 2*pow(y1,7)*z1dot + pow(y1,5)*(r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + 3*(pow(r,2) - 2*pow(z1,2))*z1dot) + 3*pow(y1,4)*z1*((-pow(r,2) + pow(z1,2))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*z1dot) + pow(z1,3)*((-pow(r,4) + pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(pow(r,2) + 2*pow(z1,2))*z1dot) + pow(y1,2)*z1*(3*(pow(r,4) - pow(r,2)*pow(z1,2) + pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(-3*pow(r,2) + 5*pow(z1,2))*z1dot) + y1*pow(z1,2)*(3*pow(r,3)*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (3*pow(r,4) - 2*pow(z1,4))*z1dot) - pow(y1,3)*(r*(pow(r,2) - pow(z1,2))*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (pow(r,4) - 3*pow(r,2)*pow(z1,2) + 6*pow(z1,4))*z1dot))/(pow(-pow(r,2) + pow(y1,2) + pow(z1,2),1.5)*pow(r*y1 + z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),3));
-  Jdot(2,0) = z1dot/pow(z1,2);
+  Jdot(1,1) = (2*((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) - r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)))*(y1*y1dot + z1*z1dot) - (pow(y1,2) + pow(z1,2))*(2*y1*z1*y1dot - r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (-pow(r,2) + pow(y1,2))*z1dot + 3*pow(z1,2)*z1dot - (r*y1*(y1*y1dot + z1*z1dot))/sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))))/pow((-pow(r,2) + pow(y1,2))*z1 + pow(z1,3) - r*y1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),2);
+  Jdot(1,2) = (-(pow(y1,6)*z1*y1dot) + 2*pow(y1,7)*z1dot + pow(y1,5)*(r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot - 3*(pow(r,2) - 2*pow(z1,2))*z1dot) + 3*pow(y1,4)*z1*((pow(r,2) - pow(z1,2))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*z1dot) + pow(z1,3)*((pow(r,4) - pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(pow(r,2) + 2*pow(z1,2))*z1dot) + pow(y1,2)*z1*(-3*(pow(r,4) - pow(r,2)*pow(z1,2) + pow(z1,4))*y1dot + r*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*(-3*pow(r,2) + 5*pow(z1,2))*z1dot) + y1*pow(z1,2)*(3*pow(r,3)*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (-3*pow(r,4) + 2*pow(z1,4))*z1dot) + pow(y1,3)*(r*(-pow(r,2) + pow(z1,2))*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2))*y1dot + (pow(r,4) - 3*pow(r,2)*pow(z1,2) + 6*pow(z1,4))*z1dot))/(pow(-pow(r,2) + pow(y1,2) + pow(z1,2),1.5)*pow(-(r*y1) + z1*sqrt(-pow(r,2) + pow(y1,2) + pow(z1,2)),3));
+  Jdot(2,0) = -(z1dot/pow(z1,2));
   Jdot(2,1) = 0;
-  Jdot(2,2) = (z1*x1dot - 2*x1*z1dot)/pow(z1,3);
+  Jdot(2,2) = (-(z1*x1dot) + 2*x1*z1dot)/pow(z1,3);
+
+  // Fix the convention
+  Jdot = Jdot * Rcc;
 }
 
 void hover_at(const geometry_msgs::Point goal)
@@ -604,7 +771,28 @@ int main(int argc, char **argv)
   n.param("state_control/safety_catch", safety, true);
   n.param("mass", mass_, 0.5);
 
-  // Publishers
+  // Params needed for so3 control from vision
+  n.param("use_external_yaw", use_external_yaw_, true);
+
+  double kR[3], kOm[3];
+  n.param("gains/rot/x", kR[0], 1.5);
+  n.param("gains/rot/y", kR[1], 1.5);
+  n.param("gains/rot/z", kR[2], 1.0);
+  n.param("gains/ang/x", kOm[0], 0.13);
+  n.param("gains/ang/y", kOm[1], 0.13);
+  n.param("gains/ang/z", kOm[2], 0.1);
+  kR_[0] = kR[0], kR_[1] = kR[1], kR_[2] = kR[2];
+  kOm_[0] = kOm[0], kOm_[1] = kOm[1], kOm_[2] = kOm[2];
+
+  double corrections[3];
+  n.param("corrections/kf", corrections[0], 0.0);
+  n.param("corrections/r", corrections[1], 0.0);
+  n.param("corrections/p", corrections[2], 0.0);
+  corrections_[0] = corrections[0], corrections_[1] = corrections[1], corrections_[2] = corrections[2];
+
+  /////////////////
+  // Publishers //
+  ///////////////
   srv_transition_= n.serviceClient<controllers_manager::Transition>("controllers_manager/transition");
   pub_goal_min_jerk_ = n.advertise<geometry_msgs::Vector3>("controllers_manager/line_tracker_min_jerk/goal", 1);
   pub_goal_distance_ = n.advertise<geometry_msgs::Vector3>("controllers_manager/line_tracker_distance/goal", 1);
@@ -614,6 +802,7 @@ int main(int argc, char **argv)
   // pub_info_bool_ = n.advertise<std_msgs::Bool>("traj_signal", 1);
   pub_motors_ = n.advertise<std_msgs::Bool>("motors", 1);
   pub_estop_ = n.advertise<std_msgs::Empty>("estop", 1);
+  so3_command_pub_ = n.advertise<quadrotor_msgs::SO3Command>("so3_cmd", 10);
 
   // Subscribers
   ros::Subscriber sub_odom = n.subscribe("odom", 1, &odom_cb, ros::TransportHints().tcpNoDelay());
@@ -625,18 +814,4 @@ int main(int argc, char **argv)
   ros::spin();
 
   return 0;
-}
-
-void color(std::string &str)
-{
-  str.insert(0, "\e[0;33m");
-  str.append("\e[0m");
-}
-
-void fmtVec3d(std::string &str, const Eigen::Vector3d &v)
-{
-  std::stringstream ss;
-  ss << str << ": {" << v(0) << ", " << v(1) << ", " << v(2) << "}";
-  str = ss.str();
-  color(str);
 }
